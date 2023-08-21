77381f20cce922351ca751c3df85e185e706294f
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-08-21 13:15:41 -0300

    FIX: erro app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
-import { EstiloGlobal, Container } from './styles'
+import EstiloGlobal, { Container } from './global'
 
 function App() {
   return (
     <>
-      <EstiloGlobal>
-        <Header />
-        <Hero />
-        <Container>
-          <ListaVagas />
-        </Container>
-      </EstiloGlobal>
+      <EstiloGlobal />
+      <Header />
+      <Hero />
+      <Container>
+        <ListaVagas />
+      </Container>
     </>
   )
 }
